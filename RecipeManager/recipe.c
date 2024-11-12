e9b00b2ee505166f9168f1d3a713ca81669da4b0
SAM PELOT <sampelot93@gmail.com>
2024-11-12 14:48:05 -0500

    Commit #1.2 - Added: recipe.c

---
+++ RecipeManager/recipe.c
@@ -0,0 +1,23 @@
+// Group 3: Recipe Manager - Sam, Johan, Ridha
+// implementation for recipe
+
+#include "recipe.h"
+
+
+//C
+// 1. add a new appt/task/recipe/other
+// 9. save accumulated data to disk
+
+//R
+// 4. display single appt/task/recipe/other
+// 5. display range appt/task/recipe/other
+// 6. display all appt/task/recipe/other
+// 7. search for appt / task / recipe / other
+// 10. load accumulated data from disk
+
+
+//U
+// 3. update an existing appt/task/recipe/other
+
+//D
+// 2. delete an existing appt/task/recipe/other
