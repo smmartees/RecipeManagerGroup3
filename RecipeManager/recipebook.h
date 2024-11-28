82d8998ab3a53c10a4eb8c2ff4026b1b37ae6cd4
SAM PELOT <sampelot93@gmail.com>
2024-11-28 10:02:01 -0500

    Sam Commit #3 - Organizing code and tasks

--- RecipeManager/recipebook.h
+++ RecipeManager/recipebook.h
@@ -20,7 +20,17 @@ void DisplayRecipebook(PRECIPEBOOK Book);
 
 void DestroyRecipeBook(PRECIPEBOOK* Book);
 
+// 4. display single recipe
+// 5. display range recipe
+// 6. display all recipe
 
-// 1. add a new recipe
-// 9. save accumulated data to disk (write file)
+// 1. add a new recipe. 2 parts:
+// -1 create recipe
+// -2 add to recipe book.
+// 9. save accumulated data to disk (write file) 4 parts:
+// -1 fprintf recipe
+// -2 Open file
+// -3 write loop display recipe through all recipes
+// - free memory? 2 save functions? One that just saves, another that saves and frees on exit?
+// -4 close file
 // 10. load accumulated data from disk (read from file - Create linked list)
