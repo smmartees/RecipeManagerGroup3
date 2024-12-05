d75b849dd4acb7a6d4f6762c6478d31c8de62d50
johannTom <37807657+johannTom@users.noreply.github.com>
2024-12-05 10:13:33 -0500

    create recipe(V1)

--- RecipeManager/recipe.c
+++ RecipeManager/recipe.c
@@ -10,11 +10,23 @@ RECIPE CreateRecipe(char* RecipeName, char Ingredients[MAX_LINES][MAX_LENGTH], c
 	strncpy(newRecipe.recipeName,RecipeName, MAX_LENGTH);
     newRecipe.recipeName[MAX_LENGTH - 1] = '\0';
 
-     
-    for (int i = 0; i < MAX_LINES; i++) {
-        strncpy(newRecipe.ingredients[i], Ingredients[i], MAX_LENGTH - 1);
-        newRecipe.ingredients[i][MAX_LENGTH - 1] = '\0'; // Ensure null-termination
-    }
+RECIPE CreateRecipe(char* RecipeName) {
+	
+	RECIPE newRecipe = {0};
+
+	strncmp(newRecipe.recipeName, RecipeName, MAX_LENGTH - 1);
+	newRecipe.recipeName[MAX_LENGTH - 1] = "\0";
+	
+	// Initialize ingredients and instructions as NULL
+
+	//newRecipe.ingredients = NULL;
+	//newRecipe.instructions = NULL;
+
+	// Set the default meal type (can update later)
+	newRecipe.type = BREAK;
+
+	return newRecipe;
+}
 
      
     for (int i = 0; i < MAX_LINES; i++) {
